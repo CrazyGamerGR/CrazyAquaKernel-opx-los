86996be4846b5b5be473ebaaf591032194e7e92c
FrancescoCG <frank-97@hotmail.gr>
2016-12-30 15:50:16 +0200

    Modify the versioning code

--- init/version.c
+++ init/version.c
@@ -41,10 +41,10 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " (CrazyGamer)"
+	"Linux version " UTS_RELEASE " (CrazySuperKernel)"
 	"(" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (CrazyGamer)"
+	" (CrazySuperKernel)"
 	" (" LINUX_COMPILER ") %s\n";
