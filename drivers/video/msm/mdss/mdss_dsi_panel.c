c48f177e4301cb7a731fe52240218c07923d3e19
FrancescoCG <frank-97@hotmail.gr>
2016-12-31 16:40:57 +0200

    Revert "Revert "kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.7 (faux123/Yank555.lu)""
    This reverts commit 5db23269cf5f7611c6d86ec1f0996ac777158b07.

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -48,6 +48,11 @@
 extern  int lm3630_bank_a_update_status(u32 bl_level);
 extern int push_component_info(enum COMPONENT_TYPE type, char *version, char * manufacture);
 #endif
+
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 
 #define MIN_REFRESH_RATE 30
@@ -859,6 +864,10 @@ static int mdss_dsi_panel_partial_update(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 	mipi  = &pdata->panel_info.mipi;
@@ -1100,6 +1109,11 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	if (ctrl->off_cmds.cmd_cnt)
         mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds);
 #endif
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
+
 	pr_debug("%s:-\n", __func__);
 	return 0;
 }
